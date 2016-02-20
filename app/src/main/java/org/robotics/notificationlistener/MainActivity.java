3d67bd608b023dd47be3e69ceae7d2c312bd4c1c
cheskett <cheskett@purdue.edu>
2016-02-20 09:57:53 -0600

    google maps parsing works

--- app/src/main/java/org/robotics/notificationlistener/MainActivity.java
+++ app/src/main/java/org/robotics/notificationlistener/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.util.Log;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,14 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
     TableLayout tab;
 
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tab = (TableLayout) findViewById(R.id.tab);
-        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
+        final IntentFilter mIntentFilter = new IntentFilter(MyAccessibilityService.Constants.ACTION_CATCH_NOTIFICATION);
+        mIntentFilter.addAction(MyAccessibilityService.Constants.ACTION_CATCH_TOAST);
+        //mIntentFilter.addAction("Msg");
+        registerReceiver(onNotice, mIntentFilter);
+        Log.v(TAG, "Receiver registered.");
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, mIntentFilter /*new IntentFilter("Msg")*/);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(onNotice);
     }
 
     private BroadcastReceiver onNotice = new BroadcastReceiver() {
@@ -35,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
             String pack = intent.getStringExtra("package");
             String title = intent.getStringExtra("title");
             String text = intent.getStringExtra("text");
+
+            if (pack == null)
+                    pack = intent.getStringExtra(MyAccessibilityService.Constants.EXTRA_PACKAGE);
+            if (text == null)
+                    text =  intent.getStringExtra(MyAccessibilityService.Constants.EXTRA_MESSAGE);
 
             Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
 
@@ -47,8 +65,14 @@ public class MainActivity extends AppCompatActivity {
             textview.setText(Html.fromHtml(pack + "<br><b>" + title + " : </b>" + text));
             tr.addView(textview);
             tab.addView(tr);
+            Toast.makeText(getApplicationContext(),"TEESTSETESTSET",Toast.LENGTH_SHORT);
+
+            Log.v(TAG, "Received message");
+            Log.v(TAG, "intent.getAction() :: " + intent.getAction());
+            Log.v(TAG, "intent.getStringExtra(Constants.EXTRA_PACKAGE) :: " + intent.getStringExtra(MyAccessibilityService.Constants.EXTRA_PACKAGE));
+            Log.v(TAG, "intent.getStringExtra(Constants.EXTRA_MESSAGE) :: " + intent.getStringExtra(MyAccessibilityService.Constants.EXTRA_MESSAGE));
         }
-
-
     };
 }
+
+
